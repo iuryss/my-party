390b74684f59ced7c25b3329e7867e384deaddd9
Carlos <carloscauantecnico@gmail.com>
2023-09-11 13:52:41 -0300

    start develop

--- src/app/components/CardSlider/cardSlide.js
+++ src/app/components/CardSlider/cardSlide.js
@@ -1,8 +1,8 @@
-'use client';
+'use client'
 
 import React, { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 import styles from '../../styles/cardSlide.module.css';
-import Image from 'next/image';
 
 const CardSlider = ({ photos }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -15,26 +15,38 @@ const CardSlider = ({ photos }) => {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? photos.length - 1 : prevSlide - 1));
   };
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => nextSlide(),
+    onSwipedRight: () => prevSlide(),
+  });
+
   return (
     <div className={styles.cardSlider}>
       <div className={styles.card}>
-        <img className={styles.cardImage} src={photos[currentSlide]} alt="Slide" />
+      <div {...handlers}>
+        <img
+          className={styles.cardImage}
+          src={photos[currentSlide]}
+          alt="Slide"
+        />
         <button className={styles.prevButton} onClick={prevSlide}>
           Previous
         </button>
         <button className={styles.nextButton} onClick={nextSlide}>
           Next
         </button>
+        
+        </div>
       </div>
       <div className={styles.dots}>
-          {photos.map((_, index) => (
-            <span
-              key={index}
-              className={`${index === currentSlide ? styles.dotSelected : styles.dot}`}
-              onClick={() => setCurrentSlide(index)}
-            />
-          ))}
-        </div>
+        {photos.map((_, index) => (
+          <span
+            key={index}
+            className={`${index === currentSlide ? styles.dotSelected : styles.dot}`}
+            onClick={() => setCurrentSlide(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
